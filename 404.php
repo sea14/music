9b33cafa2e3730d6b1af53817d08310952a7f3fd
Sharon <seaustin@live.unc.edu>
2014-06-07 20:37:03 -0400

    first stab at 404 page

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php head(array('bodyid'=>'home')); ?>
+ 
+	<div id="primary">
+
+		<div id="problem">
+			<p>Oops, there was an error! There must be wrong link somewhere, or something else went wrong!</p>
+		</div>
+		
+	<?php foot(); ?>
